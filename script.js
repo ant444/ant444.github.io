1f9c76053cf5c6ac740cffc096f1f3f81d603fbc
Anthony <124739034+ant444@users.noreply.github.com>
2023-11-19 00:46:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-
-
-
 function openTab(tabId) {
   // Hide all tab contents
   var tabContents = document.querySelectorAll('.tab-content');
   tabContents.forEach(function(tabContent) {
-    tabContent.classList.remove('active');
+    tabContent.style.display = 'none';
   });
+
+  // Show the selected tab content
+  document.getElementById(tabId).style.display = 'block';
 
   // Deactivate all tab buttons
   var tabButtons = document.querySelectorAll('.tab-button');
   tabButtons.forEach(function(tabButton) {
     tabButton.classList.remove('active');
   });
-
-  // Show the selected tab content
-  document.getElementById(tabId).classList.add('active');
 
   // Activate the clicked tab button
   var clickedButton = document.querySelector('[onclick="openTab(\'' + tabId + '\')"]');
