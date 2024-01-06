2b7eee601cdb9c1f386a3e6ddd6883f23c7516c3
Anthony <124739034+ant444@users.noreply.github.com>
2024-01-06 16:49:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function openTab(tabId) {
 //Start the search
 function performSearch(event) {
     if (event.keyCode === 13 || event.which === 13) {
+        event.preventDefault();
         var query = document.getElementById('searchInput').value.trim();
         var activeTab = document.querySelector('.tab-content.active');
 
