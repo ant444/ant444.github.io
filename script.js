8663a1d5327e949951feccc659d65e4a9560f3c8
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-10 16:53:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const navBar = document.querySelector("nav"),
+       menuBtns = document.querySelectorAll(".menu-icon"),
+       overlay = document.querySelector(".overlay");
+     menuBtns.forEach((menuBtn) => {
+       menuBtn.addEventListener("click", () => {
+         navBar.classList.toggle("open");
+       });
+     });
+     overlay.addEventListener("click", () => {
+       navBar.classList.remove("open");
+     });
+
+
+
+
 function openTab(tabId) {
   // Hide all tab contents
   var tabContents = document.querySelectorAll('.tab-content');
