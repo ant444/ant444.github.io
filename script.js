943cd2afb71d2a80530c0effee65de3e92c6de92
Anthony <124739034+ant444@users.noreply.github.com>
2024-05-07 19:13:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
-
+//Filename: script.js
+//Author: Anthony Nelson
+// Purpose: Functions used in JS to make website more accessible and increase functionality.
 // Initialize a variable to track the menu state
+
+// This function will open the menu. 
 function toggleMenu() {
-  console.log("Toggle menu function called!");
+  console.log("Toggle menu function called!"); // Not necessary, but good for troubleshooting. 
   var menuBar = document.getElementById('menuBar');
   menuBar.classList.toggle('show-menu');
 }
@@ -19,12 +22,12 @@ function openTab(tabName) {
   
   // Show the selected tab content
   document.getElementById(tabName).style.display = 'block';
-  window.history.pushState({}, '', '?tab=' + tabName);
+  window.history.pushState({}, '', '?tab=' + tabName); // changes URL to the current tab name. 
 }
 // Function to close the menu when clicking outside of it
 function closeMenuOnClickOutside(event) {
   var menuBar = document.getElementById('menuBar');
-  var menuToggle = document.querySelector('.menu-toggle');
+  var menuToggle = document.querySelector('.menu-toggle'); 
   
   // Check if the clicked element is not part of the menu or menu toggle
   if (!menuBar.contains(event.target) && !menuToggle.contains(event.target)) {
@@ -34,7 +37,8 @@ function closeMenuOnClickOutside(event) {
 
 // Add event listener to close menu on click outside
 document.addEventListener('click', closeMenuOnClickOutside);
-  
+
+// Function to toggleDyslexicUI for website. Keeps all tabs on this setting. 
 function toggleDyslexicUI() {
   var elements = document.querySelectorAll('body, header, footer, .tab-content'); // Include .tab-content elements
 
